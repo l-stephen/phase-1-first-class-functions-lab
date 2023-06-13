2fc04c41887cafddffcc93807c4a56a8f72ddd83
stephenlambertfs <stephenolambert@gmail.com>
2023-06-13 12:30:35 -0600

    finshed my solution

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const names = ["Stephen", "Sarah", "John", "David"]
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2)
+};
+console.log(returnFirstTwoDrivers(names))
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2)
+}
+console.log(returnLastTwoDrivers(names))
+
+const selectingDrives = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        return multiplyValue * value
+    }
+}
+const quad = createFareMultiplier(2)
+console.log(quad(4))
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, driverToReturn){
+    return driverToReturn(drivers)
+}
+
+console.log(selectDifferentDrivers(names, returnLastTwoDrivers))
